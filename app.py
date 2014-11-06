caa2e929261e732dda1a23ae2dc8d590f9992f41
David Wiesen <iceweasel@Davids-MacBook-Air.local>
2014-11-06 07:50:25 -0800

    Use a template

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world!"
+    return render_template('hello.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
